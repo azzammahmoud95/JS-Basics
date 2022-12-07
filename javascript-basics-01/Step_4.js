21d954bd0b9cb69bbfff2d2ea90d2ab84c927f90
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-07 14:20:51 +0200

    completed step_4 JS

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+let name = prompt("Please, Enter your name: ");
+let surname = prompt("Please, Enter your surname: ");
+let city = prompt("Enter Your city");
+
+window.alert("Name: " + name + "\n" +"Surname: " + surname + "\n" +"City: " + city);
