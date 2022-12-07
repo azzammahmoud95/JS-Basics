0c85d37812ae4b1e607ccce5eb4d955679507322
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-07 15:58:34 +0200

    edit JS1 Step_5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,7 @@
-let num1 = parseInt(prompt("Enter the First number: "));
-let num2 = parseInt(prompt("Enter the Second number: "));
+document.getElementById("validate").onclick = function(){
 
-window.alert(num1 * num2);
+let  num1= document.getElementById("first_number").value;
+let num2 = document.getElementById("second_number").value;
+
+alert(num1 * num2);
+}
