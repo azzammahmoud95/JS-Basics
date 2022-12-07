3e614aa5ae69721757979ae3a9615a70db787e28
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-07 16:24:25 +0200

    Final edit JS1 Step_6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -2,6 +2,7 @@ document.getElementById("validate").onclick = function(){
 
     let  num1= document.getElementById("first_number").value;
     let num2 = document.getElementById("second_number").value;
-    
-    alert(num1 % num2);
+    div = num1 / num2;
+    rest = num1 % num2;
+    alert("Division: " + div + "\n" + "Rest Of division : " +rest);
     }
